17e68e2515883d0187f4a608fd5c4d464851e476
Robert Chapman <rc@interbaun.com>
2022-05-27 01:58:33 -0600

    DCP decode updates

--- decode_dcp.py
+++ decode_dcp.py
@@ -112,7 +112,6 @@ settings = {
 34: 'C1 Scaled Reading',
 35: 'SE1 Raw Reading',
 36: 'C1 Raw Reading',
-45: 'Operation Mode',
 46: 'Port Protocols',
 50: 'RS-232 Baud Rate',
 55: 'RS-232 Parity',
@@ -121,32 +120,30 @@ settings = {
 75: 'CS I/O SDC Address',
 95: 'Station Source Address',
 100: 'Destination Address',
-105: 'Path Service Request Enabled',
+105: 'Add Path Service Enabled',
 110: 'Add Destination Address',
 130: 'Hop Limit',
+151: 'TDMA Slot Length',
+152: 'TDMA Slot Start Offset',
 155: 'TDMA Frame Length',
 156: 'FEC Mode',
 157: 'Enable TDMA',
 158: 'Center Transmission',
 159: 'TDMA Slot Overrun Behavior',
-160: 'TDMA Slot Length',
-161: 'TDMA Slot Start Offset',
 162: 'TDMA Slot Padding',
-163: 'TDMA Bytes Remaining',
 164: 'Encryption Key Rotation Time',
 165: 'Encrypt Outgoing Messages',
 166: 'Encryption Set Key',
 168: 'Encryption EMID',
 169: 'Encryption Key Status',
-170: 'GPS Power On Interval',
+170: 'GPS Update Period',
 171: 'Encryption Source Address To Configure',
 172: 'Encryption Remove Key',
-173: 'Status',
-175: 'GPS On Max',
+175: 'GPS Update Timeout',
 185: 'Last GPS Fix',
 186: 'Tick Lock Loop',
-190: 'Carrier Only Time',
-195: 'AGC Time',
+190: 'Carrier Only time',
+195: 'AGC time',
 200: 'RF Tail Time',
 205: 'Invert Modulation',
 210: 'Modulation Voltage',
@@ -160,6 +157,7 @@ settings = {
 267: 'Clock Status in Self Report',
 268: 'Clock Status Sensor ID',
 270: 'P1 Enable',
+280: 'TDMA Bytes Remaining',
 310: 'SE1 Mode',
 312: 'SE1 Sensor ID',
 315: 'SE1 Transmitted',
@@ -177,85 +175,82 @@ settings = {
 356: 'C1 Status',
 357: 'P1 Transmitted',
 358: 'TBR Accumulator',
-359: 'SDI-12 Tx Monitor',
-360: 'SDI-12 Tx Config',
 }
+
 
 # add names and return a tuple; can elements be named?
 class dcu_setting:
-    def __init__(self,id,length): self.id = id; self.length = length
-
-AGC_time = dcu_setting(195, 2)
-Add_Destination_Address = dcu_setting(110, 1)
-Battery = dcu_setting(30, 4)
-C1_Mode = dcu_setting(355, 1)
-C1_Raw_Reading = dcu_setting(36, 4)
-C1_Scaled_Reading = dcu_setting(34, 4)
-C1_Sensor_ID = dcu_setting(347, 1)
-C1_Status = dcu_setting(356, 4)
-C1_Transmitted = dcu_setting(317, 4)
-CS_IO_SDC_Address = dcu_setting(75, 1)
-Carrier_Only_time = dcu_setting(190, 2)
-Center_Transmission = dcu_setting(158, 1)
-Clock_Status_Sensor_ID = dcu_setting(268, 1)
-Clock_Status_in_Self_Report = dcu_setting(267, 1)
-Configuration_Sensor_Scan_Interval = dcu_setting(262, 2)
-Destination_Address = dcu_setting(100, 2)
-Enable_TDMA = dcu_setting(157, 1)
-Encrypt_Outgoing_Messages = dcu_setting(165, 1)
-Encryption_EMID = dcu_setting(168, 4)
-Encryption_Key_Rotation_Time = dcu_setting(164, 4)
-Encryption_Key_Status = dcu_setting(169, 10)
-Encryption_Remove_Key = dcu_setting(172, 1)
-Encryption_Set_Key = dcu_setting(166, 33)
-Encryption_Source_Address_To_Configure = dcu_setting(171, 2)
-FEC_Mode = dcu_setting(156, 1)
-GPS_On_Max = dcu_setting(175, 2)
-GPS_Power_On_Interval = dcu_setting(170, 2)
-Hop_Limit = dcu_setting(130, 1)
-Invert_Modulation = dcu_setting(205, 1)
-Last_GPS_Fix = dcu_setting(185, 30)
-Modulation_Voltage = dcu_setting(210, 2)
-MultiSensor_Report = dcu_setting(255, 1)
+    def __init__(self, id, length): self.id = id; self.length = length
 OS_Version = dcu_setting(1, 40)
-P1_Enable = dcu_setting(270, 1)
+Battery = dcu_setting(30, 4)
+SE1_Scaled_Reading = dcu_setting(32, 4)
 P1_Total = dcu_setting(33, 2)
-P1_Transmitted = dcu_setting(357, 2)
-Path_Service_Request_Enabled = dcu_setting(105, 1)
+C1_Scaled_Reading = dcu_setting(34, 4)
+SE1_Raw_Reading = dcu_setting(35, 4)
+C1_Raw_Reading = dcu_setting(36, 4)
 Port_Protocols = dcu_setting(46, 40)
-RF_Tail_Time = dcu_setting(200, 2)
 RS232_Baud_Rate = dcu_setting(50, 1)
-RS232_HW_Flow_Control = dcu_setting(61, 1)
 RS232_Parity = dcu_setting(55, 1)
 RS232_Stop_Bits = dcu_setting(60, 1)
+RS232_HW_Flow_Control = dcu_setting(61, 1)
+CS_IO_SDC_Address = dcu_setting(75, 1)
+Station_Source_Address = dcu_setting(95, 2)
+Destination_Address = dcu_setting(100, 2)
+Add_Path_Service_Enabled = dcu_setting(105, 1)
+Add_Destination_Address = dcu_setting(110, 1)
+Hop_Limit = dcu_setting(130, 1)
+TDMA_Slot_Length = dcu_setting(151, 4)
+TDMA_Slot_Start_Offset = dcu_setting(152, 4)
+TDMA_Frame_Length = dcu_setting(155, 4)
+FEC_Mode = dcu_setting(156, 1)
+Enable_TDMA = dcu_setting(157, 1)
+Center_Transmission = dcu_setting(158, 1)
+TDMA_Slot_Overrun_Behavior = dcu_setting(159, 1)
+TDMA_Slot_Padding = dcu_setting(162, 2)
+Encryption_Key_Rotation_Time = dcu_setting(164, 4)
+Encrypt_Outgoing_Messages = dcu_setting(165, 1)
+Encryption_Set_Key = dcu_setting(166, 33)
+Encryption_EMID = dcu_setting(168, 4)
+Encryption_Key_Status = dcu_setting(169, 10)
+GPS_Update_Period = dcu_setting(170, 2)
+Encryption_Source_Address_To_Configure = dcu_setting(171, 2)
+Encryption_Remove_Key = dcu_setting(172, 1)
+GPS_Update_Timeout = dcu_setting(175, 2)
+Last_GPS_Fix = dcu_setting(185, 30)
+Tick_Lock_Loop = dcu_setting(186, 40)
+Carrier_Only_time = dcu_setting(190, 2)
+AGC_time = dcu_setting(195, 2)
+RF_Tail_Time = dcu_setting(200, 2)
+Invert_Modulation = dcu_setting(205, 1)
+Modulation_Voltage = dcu_setting(210, 2)
 Radio_Power_Up_Mode = dcu_setting(215, 1)
 Radio_Warm_Up = dcu_setting(220, 4)
+MultiSensor_Report = dcu_setting(255, 1)
+Self_Report_Interval = dcu_setting(257, 4)
+Sensor_Scan_Interval = dcu_setting(260, 2)
+Configuration_Sensor_Scan_Interval = dcu_setting(262, 2)
+SW12_Warm_Up_Time = dcu_setting(265, 1)
+Clock_Status_in_Self_Report = dcu_setting(267, 1)
+Clock_Status_Sensor_ID = dcu_setting(268, 1)
+P1_Enable = dcu_setting(270, 1)
+TDMA_Bytes_Remaining = dcu_setting(280, 2)
+SE1_Mode = dcu_setting(310, 1)
+SE1_Sensor_ID = dcu_setting(312, 1)
+SE1_Transmitted = dcu_setting(315, 4)
+C1_Transmitted = dcu_setting(317, 4)
+SE1_Multiplier = dcu_setting(325, 4)
+SE1_Offset = dcu_setting(330, 4)
+SE1_Tx_Change = dcu_setting(340, 4)
 SDI12_Command = dcu_setting(345, 8)
+SDI12_Value_to_Send = dcu_setting(346, 1)
+C1_Sensor_ID = dcu_setting(347, 1)
 SDI12_Multiplier = dcu_setting(348, 4)
 SDI12_Offset = dcu_setting(349, 4)
 SDI12_Tx_Change = dcu_setting(350, 4)
-SDI12_Value_to_Send = dcu_setting(346, 1)
-SE1_Mode = dcu_setting(310, 1)
-SE1_Multiplier = dcu_setting(325, 4)
-SE1_Offset = dcu_setting(330, 4)
-SE1_Raw_Reading = dcu_setting(35, 4)
-SE1_Scaled_Reading = dcu_setting(32, 4)
-SE1_Sensor_ID = dcu_setting(312, 1)
-SE1_Transmitted = dcu_setting(315, 4)
-SE1_Tx_Change = dcu_setting(340, 4)
-SW12_Warm_Up_Time = dcu_setting(265, 1)
-Self_Report_Interval = dcu_setting(257, 4)
-Sensor_Scan_Interval = dcu_setting(260, 2)
-Station_Source_Address = dcu_setting(95, 2)
+C1_Mode = dcu_setting(355, 1)
+C1_Status = dcu_setting(356, 4)
+P1_Transmitted = dcu_setting(357, 2)
 TBR_Accumulator = dcu_setting(358, 2)
-TDMA_Bytes_Remaining = dcu_setting(163, 2)
-TDMA_Frame_Length = dcu_setting(155, 4)
-TDMA_Slot_Length = dcu_setting(160, 2)
-TDMA_Slot_Overrun_Behavior = dcu_setting(159, 1)
-TDMA_Slot_Padding = dcu_setting(162, 2)
-TDMA_Slot_Start_Offset = dcu_setting(161, 2)
-Tick_Lock_Loop = dcu_setting(186, 40)
-
 
 # message decoding
 RESP = 0x80
@@ -320,6 +315,7 @@ def ss_msg(msg):
     return out[:-1]
 
 def ssr_msg(msg):
+    print('ssr_msg:', msg.hex(' '))
     out = ' ' + hb(*ghr.outcome(msg)) + '\n'
     del(msg[0])
     while msg:
@@ -678,6 +674,29 @@ def float_hex(f):
 
 # Test
 if __name__ == "__main__":
+    devco = bytearray(b'\xBD\xF2\x13\x9A\x00\x00\x04\x4F\x47\xBD')
+    al200 = bytearray(b'\xBD\xF2\x93\x9A\x06\xBA\x43\xBD')
+    al200a = bytearray(b'\xBD\xF2\x93\x9A')
+    al200b = bytearray(b'\x06\xBA\x43\xBD')
+    al200c = bytearray(b'\xBD\xF2\x8F\xA2\x01\x00\x2F\x02\x01\x00\x00\xB9\x40\x10\x44\x61\x74\x65\x3A\x20\x20\x20\x20\x54\x69\x6D\x65\x3A\x20\x00\xC9\xE2\xBD')
+    al200d = bytearray(b'\xBD\xF2\x10\xA6\x00\x00\x00\xC3\x00\x02\x00\x50\xE0\x8D\xBD')
+    al200e = bytearray(b'\xBD\xF2\x0F\xA5\x00\x00\x01\x64\x01\x68\xC5\xFC\xBD')
+    al200f = bytearray(b'\xBD\xF2\x90\x83\x01\x00\x2F\x02\x01\x00\x00\xC3\x00\x02\x00\x3C\xF2\x58\xBD')
+    al200g = bytearray.fromhex('BD F2 90 C3 01 00 D7 01 21 DF BD'.strip())
+    print(decode_dcp(devco))
+    print(decode_dcp(al200))
+    print(decode_dcp(al200a))
+    print(decode_dcp(al200b))
+    print(decode_dcp(al200b))
+    print(decode_dcp(al200b))
+    print(decode_dcp(al200b))
+    print(decode_dcp(al200c))
+    print(decode_dcp(al200d))
+    print(decode_dcp(al200e))
+    print(decode_dcp(al200g))
+
+
+if __name__ == "x__main__":
     dcp = serial.Serial('/dev/cu.usbserial-1444330', baudrate=57600, timeout=0.3)
     pdu = b'\x12\x34\x56\x78\xBD\xBC'
     frame = dcp_msg()
@@ -721,24 +740,3 @@ if __name__ == "__main__":
         print(settings[set], setout[out])
 
     sys.exit(0)
-
-# if __name__ == "__main__":
-    devco = bytearray(b'\xBD\xF2\x13\x9A\x00\x00\x04\x4F\x47\xBD')
-    al200 = bytearray(b'\xBD\xF2\x93\x9A\x06\xBA\x43\xBD')
-    al200a = bytearray(b'\xBD\xF2\x93\x9A')
-    al200b = bytearray(b'\x06\xBA\x43\xBD')
-    al200c = bytearray(b'\xBD\xF2\x8F\xA2\x01\x00\x2F\x02\x01\x00\x00\xB9\x40\x10\x44\x61\x74\x65\x3A\x20\x20\x20\x20\x54\x69\x6D\x65\x3A\x20\x00\xC9\xE2\xBD')
-    al200d = bytearray(b'\xBD\xF2\x10\xA6\x00\x00\x00\xC3\x00\x02\x00\x50\xE0\x8D\xBD')
-    al200e = bytearray(b'\xBD\xF2\x0F\xA5\x00\x00\x01\x64\x01\x68\xC5\xFC\xBD')
-    al200f = bytearray(b'\xBD\xF2\x90\x83\x01\x00\x2F\x02\x01\x00\x00\xC3\x00\x02\x00\x3C\xF2\x58\xBD')
-    print(decode_dcp(devco))
-    print(decode_dcp(al200))
-    print(decode_dcp(al200a))
-    print(decode_dcp(al200b))
-    print(decode_dcp(al200b))
-    print(decode_dcp(al200b))
-    print(decode_dcp(al200b))
-    print(decode_dcp(al200c))
-    print(decode_dcp(al200d))
-    print(decode_dcp(al200e))
-    print(decode_dcp(al200f))
