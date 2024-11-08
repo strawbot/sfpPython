5921d0887f4046073e21d73aec9b9599873b68f0
Robert Chapman <rc@interbaun.com>
2024-11-08 00:19:07 -0700

    remove outside dependance
    allow repo to be used in other project structures

--- dcp_api.py
+++ dcp_api.py
@@ -1,6 +1,6 @@
 import io, traceback, sys, time, serial
 import binascii, struct
-from Pylibs.protocols.airlink import bit_fields
+from protocols.airlink import bit_fields
 
 # if __name__ == "__main__":
 #     from airlink import bit_fields
